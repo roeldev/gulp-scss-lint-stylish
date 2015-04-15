8128602eabd59a205163fba0e41f379df15d96bc
Roel Dev <roeldev@users.noreply.github.com>
2015-04-15 19:14:00 +0200

    Added basic files from my node modules starter kit

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+/**
+ * gulp-scss-lint-stylish | gulpfile.js
+ * file version: 0.00.001
+ */
+'use strict';
+
+var Gulp        = require('gulp'),
+    GulpJsHint  = require('gulp-jshint'),
+    GulpMocha   = require('gulp-mocha'),
+    RunSequence = require('run-sequence');
+
+var JS_SRC = [__filename, './lib/**/*.js', 'test/*.js'];
+
+////////////////////////////////////////////////////////////////////////////////
+
+Gulp.task('lint', function()
+{
+    return Gulp.src(JS_SRC)
+        .pipe( GulpJsHint() )
+        .pipe( GulpJsHint.reporter('jshint-stylish') );
+});
+
+Gulp.task('test', function()
+{
+    return Gulp.src('test/*.js', { 'read': false })
+        .pipe( GulpMocha() );
+});
+
+Gulp.task('dev', function()
+{
+    for(var $i = 0; $i < 30; $i++)
+    {
+        console.log('');
+    }
+
+    RunSequence('lint', 'test');
+});
+
+Gulp.task('watch', function()
+{
+    Gulp.watch(JS_SRC, ['dev']);
+});
