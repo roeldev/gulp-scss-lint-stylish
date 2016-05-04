ef58875a7e78acc0b106eabf3dfcce6aef871274
Roel Dev <roeldev@users.noreply.github.com>
2016-05-04 11:59:57 +0200

    Improvement to dev process

--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
 @echo off
-start ConEmu64 /cmdlist "npm run start" -cur_console:fn ^|^|^| cmd -cur_console:s2T40V
+start ConEmu64 /cmdlist "cmd /k npm run start" -cur_console:fn ^|^|^| cmd -cur_console:s2T40V
